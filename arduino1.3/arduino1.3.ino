06809d59e76b717216d065a28d6a89bbc04b0d48
igor2890 <igor2890@mail.ru>
2020-07-19 18:52:55 +0300

    Refactoring

--- arduino1.3/arduino1.3.ino
+++ arduino1.3/arduino1.3.ino
@@ -1,31 +1,44 @@
 #include <Servo.h>
 #include <IRremote.h>
 
-#define dvig1A0 2 //едем не едем правая PINMOTOR_RIGHT_ENABLE
-#define dvig1D7 11 //правая назад PINMOTOR_RIGHT_BACK
-#define dvig1D8 4 //правая вперед PINMOTOR_RIGHT_FORWARD
-#define dvig1D5 5 //pwm правая PINMOTOR_RIGHT_PWM
+#define PINMOTOR_RIGHT_ENABLE 2 //едем не едем правая dvig1A0
+#define PINMOTOR_RIGHT_BACK 11 //правая назад dvig1D7
+#define PINMOTOR_RIGHT_FORWARD 4 //правая вперед  dvig1D8
+#define PINMOTOR_RIGHT_PWM 5 //pwm правая dvig1D5
 
-#define dvig2A1 9 //едем не едем левая PINMOTOR_LEFT_ENABLE
-#define dvig2D9 8 //левая вперед PINMOTOR_LEFT_FORWARD
-#define dvig2D4 7 //левая назад PINMOTOR_LEFT_BACK
-#define dvig2D6 6 //pwm левая PINMOTOR_LEFT_PWM
+#define PINMOTOR_LEFT_ENABLE 9 //едем не едем левая dvig2A1
+#define PINMOTOR_LEFT_FORWARD 8 //левая вперед dvig2D9
+#define PINMOTOR_LEFT_BACK 7 //левая назад dvig2D4
+#define PINMOTOR_LEFT_PWM 6 //pwm левая dvig2D6
 
 #define PINSTEPPER_DIR 13
 #define PINSTEPPER_STEP 12
 
-#define STARTSERVANGLE 10 //10 для 50го, 15 для 51го
-#define LOWSERVANGLE 65 //65 для 50го, 80 для 51го
+#define SERVANGLE_TOP 10 //10 для 50го, 15 для 51го  
+#define SERVANGLE_BOTTOM 65 //65 для 50го, 80 для 51го
+
+//Array element numbers
+#define COMMAND_TYPE_AND_MOVE_LEFTMOTOR 0
+#define LEFT_SPEED_DIGIT_10 1
+#define LEFT_SPEED_DIGIT_1_AND_HIT_DETECT 2
+#define MOVE_RIGHTMOTOR_AND_ID_100 3
+#define RIGHT_SPEED_DIGIT_10_AND_ID_10 4
+#define RIGHT_SPEED_DIGIT_1_AND_ID_1 5
+#define MOVE_GUN 6
+#define MOVE_TOWER 7
+#define MAKE_SHOOT 8
+
+#define ID_TANK 50
 
 Servo myservo;
 IRsend irsend;
 
-char commandBuffer[50] = {0}; //создаем массив с нулями
-int IDtank = 50;     //переменная ID танка, который назначает сервер
+char commandBuffer[50] = {0};
+int IDtank = ID_TANK;
 const int stepperSpeed =  1900; //переменная скорости поворота башни в мк на 1 шаг
 const int stepperStopStep = 1750; //ограничение максимального количества шагов поворота в одну сторону
 int stepperCurrentStep = 0;
-int servoAngle = STARTSERVANGLE; //угол сервы
+int servoAngle = SERVANGLE_TOP; //угол сервы
 unsigned long TimeServo;
 unsigned long TimeStepper;
 unsigned long TimeShoot;
@@ -40,17 +53,17 @@ void setup()
   myservo.attach (10);
   myservo.write (servoAngle);
 
-  pinMode(dvig1A0, OUTPUT);   
-  pinMode(dvig1D7, OUTPUT);  
-  pinMode(dvig1D8, OUTPUT);   
+  pinMode(PINMOTOR_RIGHT_ENABLE, OUTPUT);   
+  pinMode(PINMOTOR_RIGHT_BACK, OUTPUT);  
+  pinMode(PINMOTOR_RIGHT_FORWARD, OUTPUT);   
 
-  digitalWrite(dvig1A0, LOW);
+  digitalWrite(PINMOTOR_RIGHT_ENABLE, LOW);
   
-  pinMode(dvig2A1, OUTPUT);   
-  pinMode(dvig2D9, OUTPUT);  
-  pinMode(dvig2D4, OUTPUT);   
+  pinMode(PINMOTOR_LEFT_ENABLE, OUTPUT);   
+  pinMode(PINMOTOR_LEFT_FORWARD, OUTPUT);  
+  pinMode(PINMOTOR_LEFT_BACK, OUTPUT);   
 
-  digitalWrite(dvig2A1, LOW);
+  digitalWrite(PINMOTOR_LEFT_ENABLE, LOW);
   
   pinMode(PINSTEPPER_DIR, OUTPUT);
   pinMode(PINSTEPPER_STEP, OUTPUT);
@@ -61,235 +74,293 @@ void setup()
 
 void loop()
 {
-  if (Serial.available()) {
-    controlStepper ();
+  if (Serial.available())
+  {
     int i = 0;
-    controlStepper ();
-    while (commandBuffer[i] != '#') {
-      controlStepper ();
-      if (Serial.available()) {
-        controlStepper ();
+    while (Serial.available())
+      {
+        controlTower ();
         commandBuffer[i] = Serial.read();
-        controlStepper ();
-        if (commandBuffer[i] == '#') break;
-        controlStepper ();
+        if (commandBuffer[i] == '#')
+          break;
         ++i;
-        controlStepper ();
       }
-      controlStepper ();
-    }
-    controlStepper ();
   }
+  
+  controlTower ();
 
-    switch(commandBuffer[0]){                  //если первый символ
-     case '9':
+  switch(commandBuffer[COMMAND_TYPE_AND_MOVE_LEFTMOTOR]){
+    case '9':
+      delay (500);
+      Serial.println (response_TYPE);
+      cleanCommandBuffer ();
+      break;
+    case '8':
+      {
+        IDtank = calculateTankID();
         delay (500);
-        Serial.println (response_TYPE);  //то отвечаем малинке, что мы ард.управленец
-        memset( commandBuffer, 0, sizeof( commandBuffer ) );
-        break;
-     case '8':
-       {
-        int aa = commandBuffer[3] - '0';
-        int bb = commandBuffer[4] - '0';
-        int cc = commandBuffer[5] - '0';
-        IDtank = (aa*100)+(bb*10)+cc;
-        delay (500);
-        Serial.println (response_OK);  //и отвечаем малинке, что мы получили id
-        memset( commandBuffer, 0, sizeof( commandBuffer ) );
-       }
-        break;
-      case '0':
-      case '1':
-      case '2':
-        controlStepper ();
-        controlDC ();
-        controlStepper ();
-        shoot ();
-        break;
-      case 'x':
-        deaddance ();
-        break;
-      default:
-        memset( commandBuffer, 0, sizeof( commandBuffer ) );
-        break;
-     }
-   controlStepper ();
-   controlServo ();
-} 
-
-void controlServo ()
-{
-  switch (commandBuffer[6]){
-    case '1':               //поднимаем ствол
-      if (servoAngle > STARTSERVANGLE && millis()- TimeServo > 50){
-        --servoAngle;
-        myservo.write (servoAngle);
-        TimeServo = millis();
+        Serial.println (response_OK);
+        cleanCommandBuffer ();
       }
       break;
-    case '2':               // опускаем ствол
-      if (servoAngle < LOWSERVANGLE && millis()- TimeServo > 50){
-        ++servoAngle;
-        myservo.write (servoAngle);
-        TimeServo = millis();
-      }
+    case '0':
+    case '1':
+    case '2':
+      controlTower ();
+      controlMotor ();
+      controlTower ();
+      shoot ();
+      break;
+    case 'x':
+      myservo.write (SERVANGLE_BOTTOM);
+      motionFromHit ();
+      break;
+    default:
+      cleanCommandBuffer ();
+      break;
+    }
+  controlTower ();
+  controlGun ();
+}
+
+void controlGun ()
+{
+  switch (commandBuffer[MOVE_GUN]){
+    case '1':
+      moveGunUp ();
+      break;
+    case '2':
+      moveGunDown ();
       break;
     default:
       break;
   } 
 }
 
-int controlStepper ()
+void controlTower ()
 {
-  switch (commandBuffer[7]){
-    case '1':               //поворачиваем башню направо (отрицательное значение скорости)
-      digitalWrite (PINSTEPPER_DIR , HIGH);
-      if ( (stepperCurrentStep >= stepperStopStep) || (micros()- TimeStepper <= stepperSpeed) )
-        break;
-      makeStep ();
-      ++stepperCurrentStep;
-      TimeStepper = micros();
+  switch (commandBuffer[MOVE_TOWER])
+  {
+    case '1':
+      needTurnTowerRight ();
       break;
-    case '2':               //поворачиваем башню налево (положительное значение скорости)
-      digitalWrite (PINSTEPPER_DIR , LOW);
-      if ( (stepperCurrentStep <= -(stepperStopStep)) || (micros()- TimeStepper <= stepperSpeed) )
-        break;
-      makeStep ();
-      --stepperCurrentStep;
-      TimeStepper = micros();
+    case '2':
+      needTurnTowerLeft ();
       break;
     default:
       break;
   }
 }
 
-void makeStep ()
+void controlMotor ()
 {
-      digitalWrite (PINSTEPPER_STEP , HIGH);
-      digitalWrite (PINSTEPPER_STEP , LOW);
-}
+  int digitSpeedRightDecade = commandBuffer[RIGHT_SPEED_DIGIT_10_AND_ID_10] - '0';        
+  int digitSpeedRightOne = commandBuffer[RIGHT_SPEED_DIGIT_1_AND_ID_1] - '0';
+  int motorCurrentRightSpeed = (digitSpeedRightDecade*10)+ digitSpeedRightOne;
+  motorCurrentRightSpeed = map (motorCurrentRightSpeed, 1, 99, 30, 80);
 
-void controlDC ()
-{
-  int dvigR0 = commandBuffer[4] - '0';        
-  int dvigR1 = commandBuffer[5] - '0';
-  int dvigRight = (dvigR0*10)+ dvigR1;
-  dvigRight = map (dvigRight, 1, 99, 30, 80);
-
-  int dvigL0 = commandBuffer[1] - '0';        
-  int dvigL1 = commandBuffer[2] - '0';
-  int dvigLeft = (dvigL0*10)+ dvigL1;
-  dvigLeft = map (dvigLeft, 1, 99, 30, 80);
+  int digitSpeedLeftDecade = commandBuffer[LEFT_SPEED_DIGIT_10] - '0';        
+  int digitSpeedLeftOne = commandBuffer[LEFT_SPEED_DIGIT_1_AND_HIT_DETECT] - '0';
+  int motorCurrentLeftSpeed = (digitSpeedLeftDecade*10)+ digitSpeedLeftOne;
+  motorCurrentLeftSpeed = map (motorCurrentLeftSpeed, 1, 99, 30, 80);
   
-  switch (commandBuffer[3]){         // правая
-    case '0':
-      digitalWrite(dvig1A0, LOW);     //не крутим
-      break;
+  setMotorLeftSpeed (motorCurrentLeftSpeed);
+  setMotorRightSpeed (motorCurrentRightSpeed);
+
+  switch (commandBuffer[MOVE_RIGHTMOTOR_AND_ID_100]){
     case '1':
-      digitalWrite(dvig1A0, HIGH);     // крутим правую
-      digitalWrite(dvig1D7, LOW);      // крутим правую вперед
-      digitalWrite(dvig1D8, HIGH);  
+      moveMotorRightForward ();  
       break;
     case '2':
-      digitalWrite(dvig1A0, HIGH);    // крутим правую
-      digitalWrite(dvig1D7, HIGH);    // крутим правую назад
-      digitalWrite(dvig1D8, LOW);
+      moveMotorRightBack ();
       break;
     default:
-      digitalWrite(dvig1A0, LOW);     //не крутим
+      stopMotorRight ();
       break;
   }
 
-  switch (commandBuffer[0]){        // левая
-    case '0':
-      digitalWrite(dvig2A1, LOW);     //не крутим
-      break;
+  switch (commandBuffer[COMMAND_TYPE_AND_MOVE_LEFTMOTOR]){
     case '1':
-      digitalWrite(dvig2A1, HIGH);    // крутим левую
-      digitalWrite(dvig2D9, HIGH);    // крутим левую вперед
-      digitalWrite(dvig2D4, LOW); 
+      moveMotorLeftForward ();
       break;
     case '2':
-      digitalWrite(dvig2A1, HIGH);     // крутим левую
-      digitalWrite(dvig2D9, LOW);      // крутим левую назад
-      digitalWrite(dvig2D4, HIGH);  
+      moveMotorLeftBack ();  
       break;
     default:
-      digitalWrite(dvig2A1, LOW);     //не крутим
+      stopMotorLeft ();
       break;
   }
-     analogWrite(dvig1D5, dvigRight);
-     analogWrite(dvig2D6, dvigLeft);
+
 }
 
 void shoot ()
 {
-  if ( commandBuffer[8] == '1'){
-//    Serial.println ("shoot");
+  if ( commandBuffer[MAKE_SHOOT] == '1'){
     for (int i = 0; i < 3; i++) {
       irsend.sendSony(IDtank, 12);
-      if ( commandBuffer[0] == '0' && commandBuffer[3] == '0' ) {
-        switch (i){
-          case (1):
-            digitalWrite(dvig1D7, HIGH);    // крутим правую назад
-            digitalWrite(dvig1D8, LOW);
-            digitalWrite(dvig2D9, LOW);      // крутим левую назад
-            digitalWrite(dvig2D4, HIGH);
-            break;
-          case (2):
-            digitalWrite(dvig1D7, LOW);
-            digitalWrite(dvig1D8, HIGH);
-            digitalWrite(dvig2D9, HIGH);
-            digitalWrite(dvig2D4, LOW);
-            break;
-          default:
-            break;
-        }
-        digitalWrite(dvig1A0, HIGH);
-        digitalWrite(dvig2A1, HIGH);
-        analogWrite(dvig1D5, 60);
-        analogWrite(dvig2D6, 60);
-        delay(50);
-        digitalWrite(dvig1A0, LOW);
-        digitalWrite(dvig2A1, LOW);
-      }
+      motionOnRecoil (i);
       delay(50);
     }
-  commandBuffer[8] = '0';
-  while (Serial.available() != 0)
-    {
-      Serial.read();
-//      Serial.println ("stiraem_shoot");
-    }
+  commandBuffer[MAKE_SHOOT] = '0';
+  cleanSerialBuffer ();
   }
 }
 
-void deaddance ()
+int calculateTankID ()
 {
-  if ( commandBuffer[2] == 'y'){
-//    Serial.println ("dead");
-      digitalWrite(dvig1A0, LOW);
-      digitalWrite(dvig2A1, LOW);
-      myservo.write (LOWSERVANGLE);
-      delay(500);
-      digitalWrite(dvig1D7, HIGH);    // крутим правую назад
-      digitalWrite(dvig1D8, LOW);
-      digitalWrite(dvig2D9, LOW);      // крутим левую назад
-      digitalWrite(dvig2D4, HIGH);
-      digitalWrite(dvig1A0, HIGH);
-      digitalWrite(dvig2A1, HIGH);
-      analogWrite(dvig1D5, 30);
-      analogWrite(dvig2D6, 30);
-      delay (3000);
-      digitalWrite(dvig1A0, LOW);
-      digitalWrite(dvig2A1, LOW);
-      delay (1500);
-      memset( commandBuffer, 0, sizeof( commandBuffer ) );
-      while (Serial.available() != 0)
-      {
-        Serial.read();
-//        Serial.println ("stiraem_dead");
-      }
+  int firstDigit = commandBuffer[MOVE_RIGHTMOTOR_AND_ID_100] - '0';
+  int secondDigit = commandBuffer[RIGHT_SPEED_DIGIT_10_AND_ID_10] - '0';
+  int thirdDigit = commandBuffer[RIGHT_SPEED_DIGIT_1_AND_ID_1] - '0';
+  int ID = (firstDigit*100)+(secondDigit*10)+thirdDigit;
+  return ID;
+}
 
+void motionFromHit ()
+{
+  if ( commandBuffer[LEFT_SPEED_DIGIT_1_AND_HIT_DETECT] == 'y'){
+    stopMotorLeft ();
+    stopMotorRight ();
+    delay(500);
+    moveMotorRightBack ();
+    moveMotorLeftBack ();
+    setMotorLeftSpeed (30);
+    setMotorRightSpeed (30);
+    delay (3000);
+    stopMotorLeft ();
+    stopMotorRight ();
+    delay (1500);
+    cleanSerialBuffer (); 
+  }
+}
+
+void moveGunUp ()
+{
+  if (servoAngle > SERVANGLE_TOP && millis()- TimeServo > 50)
+  {
+    --servoAngle;
+    myservo.write (servoAngle);
+    TimeServo = millis();
+  }
+}
+
+void moveGunDown ()
+{
+  if (servoAngle < SERVANGLE_BOTTOM && millis()- TimeServo > 50)
+  {
+    ++servoAngle;
+    myservo.write (servoAngle);
+    TimeServo = millis();
+  }
+}
+
+void makeStepTower ()
+{
+  digitalWrite (PINSTEPPER_STEP , HIGH);
+  digitalWrite (PINSTEPPER_STEP , LOW);
+}
+
+void moveMotorRightForward ()
+{
+  digitalWrite(PINMOTOR_RIGHT_BACK, LOW);
+  digitalWrite(PINMOTOR_RIGHT_FORWARD, HIGH);
+  digitalWrite(PINMOTOR_RIGHT_ENABLE, HIGH);
+}
+
+void moveMotorRightBack ()
+{
+  digitalWrite(PINMOTOR_RIGHT_BACK, HIGH);
+  digitalWrite(PINMOTOR_RIGHT_FORWARD, LOW);
+  digitalWrite(PINMOTOR_RIGHT_ENABLE, HIGH);
+}
+
+void moveMotorLeftForward ()
+{
+  digitalWrite(PINMOTOR_LEFT_FORWARD, HIGH);
+  digitalWrite(PINMOTOR_LEFT_BACK, LOW);
+  digitalWrite(PINMOTOR_LEFT_ENABLE, HIGH);
+}
+
+void moveMotorLeftBack ()
+{
+  digitalWrite(PINMOTOR_LEFT_FORWARD, LOW);
+  digitalWrite(PINMOTOR_LEFT_BACK, HIGH);  
+  digitalWrite(PINMOTOR_LEFT_ENABLE, HIGH);
+}
+
+void stopMotorRight ()
+{
+  digitalWrite(PINMOTOR_RIGHT_ENABLE, LOW);
+}
+
+void stopMotorLeft ()
+{
+  digitalWrite(PINMOTOR_LEFT_ENABLE, LOW);
+}
+
+void setMotorLeftSpeed (int speed)
+{
+  analogWrite(PINMOTOR_LEFT_PWM, speed);
+}
+
+void setMotorRightSpeed (int speed)
+{
+  analogWrite(PINMOTOR_RIGHT_PWM, speed);
+}
+
+void cleanCommandBuffer ()
+{
+  memset( commandBuffer, 0, sizeof( commandBuffer ) );
+}
+
+void cleanSerialBuffer ()
+{
+  while (Serial.available() != 0)
+  {
+    Serial.read();
+  }
+}
+
+void motionOnRecoil (int i)
+{
+  if ( commandBuffer[COMMAND_TYPE_AND_MOVE_LEFTMOTOR] == '0' && commandBuffer[MOVE_RIGHTMOTOR_AND_ID_100] == '0' ) {
+    switch (i){
+      case (1):
+        moveMotorRightBack ();
+        moveMotorLeftBack ();
+        break;
+      case (2):
+        moveMotorRightForward ();
+        moveMotorLeftForward ();
+        break;
+      default:
+        break;
+    }
+    setMotorLeftSpeed (60);
+    setMotorRightSpeed (60);
+    delay(50);
+    stopMotorLeft ();
+    stopMotorRight ();
+  }
+}
+
+void needTurnTowerRight ()
+{
+  if ( !(stepperCurrentStep >= stepperStopStep) && !(micros()- TimeStepper <= stepperSpeed) )
+  {  
+    digitalWrite (PINSTEPPER_DIR , HIGH);
+    makeStepTower ();
+    ++stepperCurrentStep;
+    TimeStepper = micros();
+  }
+}
+
+void needTurnTowerLeft ()
+{
+  if ( !(stepperCurrentStep <= -(stepperStopStep)) && !(micros()- TimeStepper <= stepperSpeed) )
+  {
+    digitalWrite (PINSTEPPER_DIR , LOW);
+    makeStepTower ();
+    --stepperCurrentStep;
+    TimeStepper = micros();
   }
 }
